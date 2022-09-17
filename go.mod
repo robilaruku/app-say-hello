51b645bc7dea27f16d0a2887e050fd56e52e4ff9
Robi Irhamni <robidream20@gmail.com>
2022-09-17 19:45:59 +0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/robilaruku/app-say-hello.git
+
+go 1.17
+
+require github.com/robilaruku/go-say-hello v1.5.0
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	golangmodulesayhello "github.com/robilaruku/go-say-hello"
+)
+
+func main() {
+	fmt.Println(golangmodulesayhello.SayHello())
+}
